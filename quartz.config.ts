688f8c3eaf05640b265156cb6c4f056ae94ac4f2
Studio Syro <yfndws@gmail.com>
2026-01-02 01:56:12 -0800

    Exclude templates and README from site build

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "peterariet.com",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "README - Using Obsidian.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
